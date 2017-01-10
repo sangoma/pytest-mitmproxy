799c1e707fde6ca5d669ba6892b4cc1ecc67285a
Terry Kerr <dkerr@sangoma.com>
2017-01-10 12:38:00 -0500

    change to returning urllib ParseResult to indicate proxy resource location; move stdout capture to a tempfile to prevent hanging

--- pytest_mitmproxy.py
+++ pytest_mitmproxy.py
@@ -3,8 +3,11 @@ import re
 import os
 import shutil
 import pytest
+import urllib
 from contextlib import contextmanager
 from subprocess import Popen, PIPE, SubprocessError
+from tempfile import TemporaryFile
+from urllib.parse import urlparse
 
 # clean the dumps each run
 if os.path.isdir("traffic_dumps"):
@@ -12,7 +15,7 @@ if os.path.isdir("traffic_dumps"):
 
 
 @contextmanager
-def new_proxy(name: str, scope: str, flt: str=None):
+def new_proxy(name: str, scope: str, flt: str=None) -> urllib.parse.ParseResult:
     ''' Start a new mitmdump process and
     return the address for clients to connect
 
@@ -27,25 +30,26 @@ def new_proxy(name: str, scope: str, flt: str=None):
     args = ["mitmdump", "--port=0", "--wfile={}".format(dump_file)]
     args.append(flt) if flt else None
 
-    ps = Popen(
-        args,
-        stdout=PIPE,
-        stderr=PIPE,
-        universal_newlines=True,
-        bufsize=1
-    )
-    if ps.poll() is not None:
-        raise SubprocessError("mitmdump was unable to run")
-    status = ps.stdout.readline()
-    addr = re.search(".*http://([^\n]+)\n", status).group(1)
-    yield addr
+    with TemporaryFile() as fp:
+
+        ps = Popen(
+            args,
+            stdout=fp,
+            universal_newlines=True,
+            bufsize=1
+        )
+        if ps.poll() is not None:
+            raise SubprocessError("mitmdump was unable to run")
+        fp.seek(0)
+        status = fp.read().decode("utf-8")
+        url = urlparse(re.search(".*(http://[^\n]+)\n", status).group(1))
+        yield url
     ps.terminate()
 
 
 @pytest.fixture
-def proxy(request):
+def proxy(request) -> urllib.parse.ParseResult:
     mark = request.node.get_marker("dump_filter")
     flt = mark.args[0] if mark else None
-    with new_proxy(request.node.name, request.scope, flt) as addr:
-        ip, port = addr.split(":")
-        yield ip, port
+    with new_proxy(request.node.name, request.scope, flt) as url:
+        yield url
